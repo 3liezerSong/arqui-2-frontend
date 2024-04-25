7b198408cb8e1fe186207f082215a7e452ee827e
Eliezer Zapeta <3liezer0abr@gmail.com>
2024-04-24 22:28:42 -0600

    new commit

--- Backend/src/MqttClient.js
+++ Backend/src/MqttClient.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const mysql = require('mysql');
 const http = require('http');
 //const socketIo = require('socket.io');
 const mqtt = require('mqtt');
@@ -12,12 +13,28 @@ const server = http.createServer(app);
 // Configuración de CORS
 app.use(cors());
 
+// Configuración de la conexión a la base de datos
+const con = mysql.createConnection({
+  host: '34.173.229.176', 
+  user: 'root', 
+  password: '9$C@lg*`npK[*3/b'
+});
+
+// Conexión a la base de datos
+con.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos:', err);
+    return;
+  }
+  console.log('Conexión exitosa a la base de datos MySQL');
+});
+
 const options = {
-  host: '782655401e1e4d25a1679a6d8e37a1c5.s1.eu.hivemq.cloud',
+  host: 'sff234f1.ala.us-east-1.emqxsl.com',
   port: 8883,
   protocol: 'mqtts',
-  username: '3liezer',
-  password: "9:782`@i7'3pLUC'"
+  username: 'grupo1_arqui2_2024',
+  password: "kCg!68cay-8uZX7"
 };
 
 const client = mqtt.connect(options);
@@ -54,6 +71,22 @@ app.get('/topicValues', (req, res) => {
 });
 
 
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor HTTP y WebSocket iniciado en el puerto ${PORT}`);
+});
+
+
+
+/*
+// Enviar mensaje MQTT desde el frontend
+socket.emit('send_mqtt_message', {
+  topic: 'mi/tema/mqtt',
+  payload: 'Mensaje MQTT desde el frontend'
+});
+
+*/
+
 //const humedad = 60; // Ejemplo de valor de humedad (deberías obtener este valor de algún sensor o fuente de datos)
 //client.publish('Humedad', humedad.toString(), (err) => {
 //  if (err) {
@@ -78,20 +111,3 @@ app.get('/topicValues', (req, res) => {
 // Servir el frontend estático
 //app.use(express.static('public'));
 // Servir el frontend estático
-
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Servidor HTTP y WebSocket iniciado en el puerto ${PORT}`);
-});
-
-
-
-/*
-// Enviar mensaje MQTT desde el frontend
-socket.emit('send_mqtt_message', {
-  topic: 'mi/tema/mqtt',
-  payload: 'Mensaje MQTT desde el frontend'
-});
-
-*/
